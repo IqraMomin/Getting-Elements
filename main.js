5d6875363eaaad8508a45e875dfd5a800310603b
IqraMomin <iqra2018momin@gmail.com>
2025-06-23 22:16:30 +0530

    querySelectors

--- main.js
+++ main.js
@@ -11,3 +11,12 @@ listItems[4].style.color = "blue";
 for (let li of listItems) {
     li.style.fontStyle = "italic";
 }
+//querySelector and querySelectorAll
+const basketHeading = document.querySelector("#basket-heading");
+basketHeading.style.color = "brown";
+
+const evenItems = document.querySelectorAll(".fruit:nth-child(even)");
+for (let v of evenItems) {
+    v.style.backgroundColor = "brown";
+    v.style.color = "white";
+}
